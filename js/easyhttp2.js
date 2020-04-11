c00642533a564289f9da68723c1c050f8f4bd1d6
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-04-11 17:01:47 +0300

    Added get method in EasyHTTP class for making get requests to rest apis

---
+++ js/easyhttp2.js
@@ -0,0 +1,10 @@
+class EasyHTTP {
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error))
+    });
+  }
+}
