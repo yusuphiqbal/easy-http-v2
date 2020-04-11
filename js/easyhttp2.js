ee6428b794f522d7bee10b6dd8ea63c01ed2b55f
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-04-11 17:08:31 +0300

    Added method for making post requests to rest apis

--- js/easyhttp2.js
+++ js/easyhttp2.js
@@ -7,4 +7,13 @@ class EasyHTTP {
         .catch(error => reject(error))
     });
   }
+
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, { method: 'POST', headers: { 'Content-type': 'Application/json' }, body: JSON.stringify(data) })
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error))
+    });
+  }
 }
